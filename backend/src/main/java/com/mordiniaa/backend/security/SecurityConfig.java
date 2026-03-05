36f850c2adc08745fe10509504382218a8741792
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-03-05 13:41:05 +0100

    Audit Added To Config

--- backend/src/main/java/com/mordiniaa/backend/security/SecurityConfig.java
+++ backend/src/main/java/com/mordiniaa/backend/security/SecurityConfig.java
@@ -1,15 +1,15 @@
 package com.mordiniaa.backend.security;
 
+import com.mordiniaa.backend.security.filters.AuditLoggingFilter;
+import com.mordiniaa.backend.security.filters.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 
@@ -23,7 +23,7 @@ import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http, JwtAuthenticationFilter jwtAuthenticationFilter, AuditLoggingFilter auditLoggingFilter) throws Exception {
         return http
                 .csrf(csrf ->
                         csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
@@ -37,6 +37,8 @@ public class SecurityConfig {
                                 .requestMatchers("/api/csrf-token").permitAll()
                                 .requestMatchers("/api/v1/test/**").permitAll()
                 )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterAfter(auditLoggingFilter, JwtAuthenticationFilter.class)
                 .build();
     }
 }
