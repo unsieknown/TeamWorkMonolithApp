7b97e59c336937eb1e4b6b290a1a34945790301a
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-03-07 11:45:51 +0100

    Service For Ban File

---
+++ backend/src/main/java/com/mordiniaa/backend/security/service/FileBanService.java
@@ -0,0 +1,55 @@
+package com.mordiniaa.backend.security.service;
+
+import com.mordiniaa.backend.config.StorageProperties;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.LinkedBlockingQueue;
+
+
+@Slf4j
+@Getter
+@Service
+@RequiredArgsConstructor
+public class FileBanService {
+
+    private final BlockingQueue<String> queue = new LinkedBlockingQueue<>();
+    private final Set<String> pending = ConcurrentHashMap.newKeySet();
+
+    private final StorageProperties storageProperties;
+
+    public List<String> loadBannedIps() {
+        Path filePath = Path.of(storageProperties.getBannedIps().getPath());
+        try {
+            if (Files.notExists(filePath)) {
+                Files.createDirectories(filePath.getParent());
+                Files.createFile(filePath);
+                return List.of();
+            }
+            return Files.readAllLines(filePath, Charset.defaultCharset());
+        } catch (IOException e) {
+            log.error("Error While Reading Banned Ips: {}", e.getMessage());
+            throw new RuntimeException();
+        }
+    }
+
+    public void enqueuePermanentBan(String ip) {
+        System.out.println("Adding To Ban");
+        if (pending.add(ip))
+            queue.offer(ip);
+    }
+
+    void removePending(String ip) {
+        pending.remove(ip);
+    }
+}
