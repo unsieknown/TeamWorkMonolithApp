b799c6ce0b731e8934b62ed04d95c5963ee5a651
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-03-08 14:55:22 +0100

    Set User Password Controller Method Implemented

--- backend/src/main/java/com/mordiniaa/backend/controllers/secured/admin/UserAdminController.java
+++ backend/src/main/java/com/mordiniaa/backend/controllers/secured/admin/UserAdminController.java
@@ -2,6 +2,7 @@ package com.mordiniaa.backend.controllers.secured.admin;
 
 import com.mordiniaa.backend.dto.user.UserDto;
 import com.mordiniaa.backend.request.user.CreateUserRequest;
+import com.mordiniaa.backend.request.user.PasswordRequest;
 import com.mordiniaa.backend.request.user.patch.PatchUserAddressRequest;
 import com.mordiniaa.backend.request.user.patch.PatchUserContactDataRequest;
 import com.mordiniaa.backend.request.user.patch.PatchUserDataRequest;
@@ -10,10 +11,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -31,6 +29,15 @@ public class UserAdminController {
     public ResponseEntity<UserDto> createUser(@Valid @RequestBody CreateUserRequest createUserRequest) {
         UserDto dto = userAdminService.createUser(createUserRequest);
         return new ResponseEntity<>(dto, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/user-pass")
+    public ResponseEntity<Void> setUserPassword(
+            @RequestParam(name = "u") UUID userId,
+            @Valid @RequestBody PasswordRequest passwordRequest
+    ) {
+        userAdminService.setUserPassword(userId, passwordRequest);
+        return ResponseEntity.ok().build();
     }
 
     public void updateUserBasicData(UUID userId, @Valid @RequestBody PatchUserDataRequest patchUserDataRequest) {
