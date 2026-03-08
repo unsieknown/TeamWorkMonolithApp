43876f950a7ce9b51f011a805ee51532e5755802
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-03-08 13:51:35 +0100

    Create User Controller

--- backend/src/main/java/com/mordiniaa/backend/controllers/secured/admin/UserAdminController.java
+++ backend/src/main/java/com/mordiniaa/backend/controllers/secured/admin/UserAdminController.java
@@ -1,11 +1,16 @@
 package com.mordiniaa.backend.controllers.secured.admin;
 
+import com.mordiniaa.backend.dto.user.UserDto;
 import com.mordiniaa.backend.request.user.CreateUserRequest;
 import com.mordiniaa.backend.request.user.patch.PatchUserAddressRequest;
 import com.mordiniaa.backend.request.user.patch.PatchUserContactDataRequest;
 import com.mordiniaa.backend.request.user.patch.PatchUserDataRequest;
+import com.mordiniaa.backend.services.user.UserAdminService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +21,16 @@ import java.util.UUID;
 @RequestMapping("/api/v1/admin/user")
 public class UserAdminController {
 
-    public void createUser(@Valid @RequestBody CreateUserRequest createUserRequest) {
+    private final UserAdminService userAdminService;
 
+    public UserAdminController(UserAdminService userAdminService) {
+        this.userAdminService = userAdminService;
+    }
+
+    @PostMapping("/create-user")
+    public ResponseEntity<UserDto> createUser(@Valid @RequestBody CreateUserRequest createUserRequest) {
+        UserDto dto = userAdminService.createUser(createUserRequest);
+        return new ResponseEntity<>(dto, HttpStatus.CREATED);
     }
 
     public void updateUserBasicData(UUID userId, @Valid @RequestBody PatchUserDataRequest patchUserDataRequest) {
