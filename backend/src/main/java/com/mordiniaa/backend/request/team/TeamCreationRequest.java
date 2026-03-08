d3de2bb1111421e2c888d941ac5e8c2f2aa333ad
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-03-08 23:20:38 +0100

    Annotatnio Deleted

--- backend/src/main/java/com/mordiniaa/backend/request/team/TeamCreationRequest.java
+++ backend/src/main/java/com/mordiniaa/backend/request/team/TeamCreationRequest.java
@@ -3,15 +3,11 @@ package com.mordiniaa.backend.request.team;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class TeamCreationRequest {
 
     @NotBlank
