89cda9c908fa741175136d6aacb8328c097586e5
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-03-08 23:20:31 +0100

    Create Team Controller Implemented

--- backend/src/main/java/com/mordiniaa/backend/controllers/secured/admin/TeamAdminController.java
+++ backend/src/main/java/com/mordiniaa/backend/controllers/secured/admin/TeamAdminController.java
@@ -1,22 +1,46 @@
 package com.mordiniaa.backend.controllers.secured.admin;
 
+import com.mordiniaa.backend.dto.team.TeamShortDto;
+import com.mordiniaa.backend.payload.ApiResponse;
 import com.mordiniaa.backend.request.team.TeamCreationRequest;
+import com.mordiniaa.backend.services.team.TeamAdminService;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/admin/team")
 public class TeamAdminController {
 
-    public void createTeam(@Valid TeamCreationRequest teamCreationRequest) {
+    private final TeamAdminService teamAdminService;
 
+    @PostMapping
+    public ResponseEntity<ApiResponse<TeamShortDto>> createTeam(@Valid @RequestBody TeamCreationRequest teamCreationRequest) {
+
+        TeamShortDto dto = teamAdminService.createTeam(teamCreationRequest);
+        ApiResponse<TeamShortDto> response = new ApiResponse<>(
+                "Success",
+                dto
+        );
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
-    public void assignManagerToTeam(UUID userId, UUID teamId) {
-
+    @PutMapping("/{teamId}")
+    public ResponseEntity<ApiResponse<TeamShortDto>> assignManagerToTeam(
+            @RequestParam("u") UUID userId,
+            @PathVariable UUID teamId
+    ) {
+        TeamShortDto dto = teamAdminService.assignManagerToTeam(userId, teamId);
+        ApiResponse<TeamShortDto> response = new ApiResponse<>(
+                "Assigned",
+                dto
+        );
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
     public void removeManagerFromTeam(UUID teamId) {
