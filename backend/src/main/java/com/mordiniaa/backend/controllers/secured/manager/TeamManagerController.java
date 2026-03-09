21b8978dcd3ebd978dd7fdbff8f3d36459ead0c3
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-03-09 13:09:09 +0100

    Controller For Teams List Implemented

--- backend/src/main/java/com/mordiniaa/backend/controllers/secured/manager/TeamManagerController.java
+++ backend/src/main/java/com/mordiniaa/backend/controllers/secured/manager/TeamManagerController.java
@@ -1,14 +1,48 @@
 package com.mordiniaa.backend.controllers.secured.manager;
 
+import com.mordiniaa.backend.dto.team.TeamShortDto;
+import com.mordiniaa.backend.payload.ApiResponse;
+import com.mordiniaa.backend.payload.CollectionResponse;
+import com.mordiniaa.backend.payload.PageMeta;
+import com.mordiniaa.backend.security.utils.AuthUtils;
+import com.mordiniaa.backend.services.team.TeamService;
+import com.mordiniaa.backend.utils.PageResult;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.UUID;
+
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/manager/team")
 public class TeamManagerController {
 
-    public void getTeams() {
+    private final AuthUtils authUtils;
+    private final TeamService teamService;
 
+
+    @GetMapping
+    public ResponseEntity<CollectionResponse<TeamShortDto>> getTeams() {
+
+        UUID managerId = authUtils.authenticatedUserId();
+        List<TeamShortDto> dtos = teamService.getTeamsForManager(managerId);
+        PageMeta pageMeta = new PageMeta(
+                0,
+                dtos.size(),
+                dtos.size(),
+                1,
+                true
+        );
+        return ResponseEntity.ok(
+                new CollectionResponse<>(
+                        dtos,
+                        pageMeta
+                )
+        );
     }
 
     public void getTeamDetails() {
