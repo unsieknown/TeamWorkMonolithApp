df67b7d8d4ec947ef61700c69619290334068140
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-03-10 00:43:30 +0100

    Get Board Details Controller

--- backend/src/main/java/com/mordiniaa/backend/controllers/global/boardsControllers/BoardsController.java
+++ backend/src/main/java/com/mordiniaa/backend/controllers/global/boardsControllers/BoardsController.java
@@ -1,15 +1,14 @@
 package com.mordiniaa.backend.controllers.global.boardsControllers;
 
+import com.mordiniaa.backend.dto.board.BoardDetailsDto;
 import com.mordiniaa.backend.dto.board.BoardShortDto;
+import com.mordiniaa.backend.payload.ApiResponse;
 import com.mordiniaa.backend.payload.CollectionResponse;
 import com.mordiniaa.backend.payload.PageMeta;
 import com.mordiniaa.backend.security.utils.AuthUtils;
 import com.mordiniaa.backend.services.board.BoardUserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 import java.util.List;
@@ -50,7 +49,18 @@ public class BoardsController {
         );
     }
 
-    public void getBoardDetails() {
+    @GetMapping("/{boardId}")
+    public ResponseEntity<ApiResponse<BoardShortDto>> getBoardDetails(
+            @RequestParam("t") UUID teamId,
+            @PathVariable String boardId) {
 
+        UUID userId = authUtils.authenticatedUserId();
+        BoardDetailsDto dto = boardUserService.getBoardDetails(userId, boardId, teamId);
+        return ResponseEntity.ok(
+                new ApiResponse<>(
+                        "Success",
+                        dto
+                )
+        );
     }
 }
