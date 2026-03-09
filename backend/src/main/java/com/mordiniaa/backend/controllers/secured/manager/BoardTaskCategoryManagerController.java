29783e749a225118e38f5c61d1fad47a178c71e6
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-03-09 23:45:54 +0100

    Create Task Category Controller

--- backend/src/main/java/com/mordiniaa/backend/controllers/secured/manager/BoardTaskCategoryManagerController.java
+++ backend/src/main/java/com/mordiniaa/backend/controllers/secured/manager/BoardTaskCategoryManagerController.java
@@ -1,20 +1,43 @@
 package com.mordiniaa.backend.controllers.secured.manager;
 
+import com.mordiniaa.backend.dto.board.BoardDetailsDto;
+import com.mordiniaa.backend.payload.ApiResponse;
 import com.mordiniaa.backend.request.board.TaskCategoryRequest;
+import com.mordiniaa.backend.security.utils.AuthUtils;
+import com.mordiniaa.backend.services.board.owner.BoardOwnerManagementService;
+import com.mordiniaa.backend.services.board.owner.BoardOwnerTaskCategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/manager/board/{boardId}/category")
 public class BoardTaskCategoryManagerController {
 
+
+    private final BoardOwnerTaskCategoryService boardOwnerTaskCategoryService;
+    private final AuthUtils authUtils;
+
     @PostMapping
-    public void createTaskCategory(
+    public ResponseEntity<ApiResponse<BoardDetailsDto>> createTaskCategory(
             @PathVariable String boardId,
             @RequestBody TaskCategoryRequest taskCategoryRequest
     ) {
 
+        UUID managerId = authUtils.authenticatedUserId();
+        BoardDetailsDto dto = boardOwnerTaskCategoryService.createTaskCategory(managerId, boardId, taskCategoryRequest);
+
+        return new ResponseEntity<>(
+                new ApiResponse<>(
+                        "Created Successfully",
+                        dto
+                ),
+                HttpStatus.CREATED
+        );
     }
 
     @PutMapping("/rename")
