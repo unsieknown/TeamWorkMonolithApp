75f6c52c93004e8e12caf5b75430343a6c6a7ec1
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-03-10 11:07:15 +0100

    Assign Users To Task Controller

--- backend/src/main/java/com/mordiniaa/backend/controllers/global/task/TaskManagementController.java
+++ backend/src/main/java/com/mordiniaa/backend/controllers/global/task/TaskManagementController.java
@@ -2,9 +2,11 @@ package com.mordiniaa.backend.controllers.global.task;
 
 import com.mordiniaa.backend.dto.task.TaskDetailsDTO;
 import com.mordiniaa.backend.payload.ApiResponse;
+import com.mordiniaa.backend.request.task.AssignUsersRequest;
 import com.mordiniaa.backend.request.task.PatchTaskDataRequest;
 import com.mordiniaa.backend.security.utils.AuthUtils;
 import com.mordiniaa.backend.services.task.TaskManagementService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,7 +21,6 @@ public class TaskManagementController {
     private final AuthUtils authUtils;
     private final TaskManagementService taskManagementService;
 
-
     @PatchMapping
     public ResponseEntity<ApiResponse<TaskDetailsDTO>> updateTask(
             @PathVariable String taskId,
@@ -33,6 +34,23 @@ public class TaskManagementController {
         return ResponseEntity.ok(
                 new ApiResponse<>(
                         "Updated Successfully",
+                        dto
+                )
+        );
+    }
+
+    @PutMapping("/user/add")
+    public ResponseEntity<ApiResponse<TaskDetailsDTO>> assignUsersToTask(
+            @PathVariable String taskId,
+            @RequestParam("b") String boardId,
+            @Valid @RequestBody AssignUsersRequest assignUsersRequest
+    ) {
+
+        UUID userId = authUtils.authenticatedUserId();
+        TaskDetailsDTO dto = taskManagementService.assignUsersToTask(userId, assignUsersRequest, boardId, taskId);
+        return ResponseEntity.ok(
+                new ApiResponse<>(
+                        "Successfully Assigned",
                         dto
                 )
         );
