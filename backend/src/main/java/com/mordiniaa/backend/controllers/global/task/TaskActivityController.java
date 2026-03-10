f12044f45c6191ced2d6d240869bcedb58cc6ede
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-03-10 12:27:26 +0100

    Change Task Position Controller

---
+++ backend/src/main/java/com/mordiniaa/backend/controllers/global/task/TaskActivityController.java
@@ -0,0 +1,40 @@
+package com.mordiniaa.backend.controllers.global.task;
+
+import com.mordiniaa.backend.dto.task.TaskShortDto;
+import com.mordiniaa.backend.payload.ApiResponse;
+import com.mordiniaa.backend.request.task.UpdateTaskPositionRequest;
+import com.mordiniaa.backend.security.utils.AuthUtils;
+import com.mordiniaa.backend.services.task.TaskActivityService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v/tasks/{taskId}/activity")
+public class TaskActivityController {
+
+    private final AuthUtils authUtils;
+    private final TaskActivityService taskActivityService;
+
+    @PutMapping("/position")
+    public ResponseEntity<ApiResponse<TaskShortDto>> changeTaskPosition(
+            @PathVariable String taskId,
+            @RequestParam("b") String boardId,
+            @Valid @RequestBody UpdateTaskPositionRequest updateTaskPositionRequest
+            ) {
+
+        UUID userId = authUtils.authenticatedUserId();
+
+        TaskShortDto dto = taskActivityService.changeTaskPosition(userId, boardId, taskId, updateTaskPositionRequest);
+        return ResponseEntity.ok(
+                new ApiResponse<>(
+                        "Updated Successfully",
+                        dto
+                )
+        );
+    }
+}
