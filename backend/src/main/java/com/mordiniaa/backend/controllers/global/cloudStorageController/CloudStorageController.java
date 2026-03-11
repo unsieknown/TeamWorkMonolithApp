1bb281dce3978b256cf002e54cf29b0236ca6bfc
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-03-11 11:45:59 +0100

    Upload Controller

--- backend/src/main/java/com/mordiniaa/backend/controllers/global/cloudStorageController/CloudStorageController.java
+++ backend/src/main/java/com/mordiniaa/backend/controllers/global/cloudStorageController/CloudStorageController.java
@@ -1,21 +1,39 @@
 package com.mordiniaa.backend.controllers.global.cloudStorageController;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.mordiniaa.backend.payload.ApiResponse;
+import com.mordiniaa.backend.security.utils.AuthUtils;
+import com.mordiniaa.backend.services.storage.cloudStorage.CloudStorageServiceCreateResource;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/v1/storage")
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/storage/resource")
 public class CloudStorageController {
 
-    @PostMapping
-    public void upload(
-            @RequestParam("file") MultipartFile file,
-            @RequestParam("parentId") UUID parentId
+    private final AuthUtils authUtils;
+    private final CloudStorageServiceCreateResource cloudStorageServiceCreateResource;
+
+    @PostMapping("/upload")
+    public ResponseEntity<ApiResponse<Void>> upload(
+            @RequestBody MultipartFile file,
+            @RequestParam(value = "parentId", required = false) UUID parentId
             ) {
+
+        UUID userId = authUtils.authenticatedUserId();
+        cloudStorageServiceCreateResource.uploadFile(userId, parentId, file);
+
+        return new ResponseEntity<>(
+                new ApiResponse<>(
+                        "Uploaded Successfully",
+                        null
+                ),
+                HttpStatus.CREATED
+        );
     }
 }
