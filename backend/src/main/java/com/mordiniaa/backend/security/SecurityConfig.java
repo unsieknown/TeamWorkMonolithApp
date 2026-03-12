43058560ec7ff53f07bb4938f04c4b8637fbfb6e
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-03-12 19:38:06 +0100

    Sec Config Extended

--- backend/src/main/java/com/mordiniaa/backend/security/SecurityConfig.java
+++ backend/src/main/java/com/mordiniaa/backend/security/SecurityConfig.java
@@ -84,7 +84,9 @@ public class SecurityConfig {
                                 .requestMatchers(
                                         HttpMethod.POST,
                                         "/api/v1/auth/signin",
-                                        "/api/v1/auth/refresh"
+                                        "/api/v1/auth/refresh",
+                                        "/api/v1/auth/forgot-password",
+                                        "/api/v1/auth/reset-password"
                                 ).permitAll()
                                 .requestMatchers(HttpMethod.GET, "/images/**").permitAll()
                                 .requestMatchers("/api/v1/test/**").permitAll()
