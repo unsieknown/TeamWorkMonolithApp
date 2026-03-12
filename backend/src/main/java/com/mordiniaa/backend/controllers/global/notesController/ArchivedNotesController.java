fcee254c12d42334e6ba00ffc9fb7a58100f69c7
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-03-12 13:09:47 +0100

    Get All Archived Notes Controller

--- backend/src/main/java/com/mordiniaa/backend/controllers/global/notesController/ArchivedNotesController.java
+++ backend/src/main/java/com/mordiniaa/backend/controllers/global/notesController/ArchivedNotesController.java
@@ -3,7 +3,9 @@ package com.mordiniaa.backend.controllers.global.notesController;
 import com.mordiniaa.backend.config.NotesConstants;
 import com.mordiniaa.backend.dto.note.NoteDto;
 import com.mordiniaa.backend.payload.CollectionResponse;
+import com.mordiniaa.backend.security.utils.AuthUtils;
 import com.mordiniaa.backend.services.notes.ArchivedNotesService;
+import com.mordiniaa.backend.utils.PageResult;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
@@ -12,6 +14,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.UUID;
+
 @Validated
 @RestController
 @RequiredArgsConstructor
@@ -19,13 +24,16 @@ import org.springframework.web.bind.annotation.*;
 public class ArchivedNotesController {
 
     private final ArchivedNotesService archivedNotesService;
+    private final AuthUtils authUtils;
 
     @GetMapping
     public ResponseEntity<CollectionResponse<NoteDto>> getAllArchivedNotesForUser(
             @RequestParam(name = "pn", required = false, defaultValue = NotesConstants.PAGE_NUMBER) @PositiveOrZero int pageNumber,
             @RequestParam(name = "ps", required = false, defaultValue = NotesConstants.PAGE_SIZE) @Positive @Max(50) int pageSize
     ) {
-        return null;
+        UUID userId = authUtils.authenticatedUserId();
+        PageResult<List<NoteDto>> result = archivedNotesService.fetchAllArchivedNotes(userId, pageNumber, pageSize);
+        return ResponseEntity.ok(new CollectionResponse<>(result.getData(), result.getPageMeta()));
     }
 
     @PutMapping("/{noteId}")
