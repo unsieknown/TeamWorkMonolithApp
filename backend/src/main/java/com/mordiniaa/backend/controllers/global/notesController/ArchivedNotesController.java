1865c81e199a71577dbbd7e0ab0f12018840fc79
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-03-12 13:15:06 +0100

    Switch Archived Status Controller

--- backend/src/main/java/com/mordiniaa/backend/controllers/global/notesController/ArchivedNotesController.java
+++ backend/src/main/java/com/mordiniaa/backend/controllers/global/notesController/ArchivedNotesController.java
@@ -2,6 +2,7 @@ package com.mordiniaa.backend.controllers.global.notesController;
 
 import com.mordiniaa.backend.config.NotesConstants;
 import com.mordiniaa.backend.dto.note.NoteDto;
+import com.mordiniaa.backend.payload.ApiResponse;
 import com.mordiniaa.backend.payload.CollectionResponse;
 import com.mordiniaa.backend.security.utils.AuthUtils;
 import com.mordiniaa.backend.services.notes.ArchivedNotesService;
@@ -37,7 +38,15 @@ public class ArchivedNotesController {
     }
 
     @PutMapping("/{noteId}")
-    public ResponseEntity<Void> switchArchived(@PathVariable String noteId) {
-        return null;
+    public ResponseEntity<ApiResponse<Void>> switchArchived(@PathVariable String noteId) {
+
+        UUID userId = authUtils.authenticatedUserId();
+        archivedNotesService.switchArchivedNoteForUser(userId, noteId);
+        return ResponseEntity.ok(
+                new ApiResponse<>(
+                        "Archive Status Switched Successfully",
+                        null
+                )
+        );
     }
 }
