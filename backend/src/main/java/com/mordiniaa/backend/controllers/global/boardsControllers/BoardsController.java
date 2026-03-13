91b94366be2eaed5993e50cd5554ba0ec717c514
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-03-13 20:17:14 +0100

    Swagger Documentatio For Board Controller

--- backend/src/main/java/com/mordiniaa/backend/controllers/global/boardsControllers/BoardsController.java
+++ backend/src/main/java/com/mordiniaa/backend/controllers/global/boardsControllers/BoardsController.java
@@ -2,32 +2,52 @@ package com.mordiniaa.backend.controllers.global.boardsControllers;
 
 import com.mordiniaa.backend.dto.board.BoardDetailsDto;
 import com.mordiniaa.backend.dto.board.BoardShortDto;
-import com.mordiniaa.backend.payload.ApiResponse;
-import com.mordiniaa.backend.payload.CollectionResponse;
-import com.mordiniaa.backend.payload.PageMeta;
+import com.mordiniaa.backend.payload.*;
+import com.mordiniaa.backend.payload.board.BoardDetailesCollectionResponse;
+import com.mordiniaa.backend.payload.board.BoardDetailsResponse;
 import com.mordiniaa.backend.security.utils.AuthUtils;
 import com.mordiniaa.backend.services.board.BoardUserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
+@Tag(
+        name = "Read APIs for Fetching Boards List Or Board Details"
+)
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/boards")
 public class BoardsController {
 
     private final AuthUtils authUtils;
     private final BoardUserService boardUserService;
 
-    public BoardsController(AuthUtils authUtils, BoardUserService boardUserService) {
-        this.authUtils = authUtils;
-        this.boardUserService = boardUserService;
-    }
-
+    @Operation(
+            summary = "Get Boards List",
+            description = "Returns List Of Short Board Dto In Collection Response"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "List Of Board Fetched", content = @Content(
+                    schema = @Schema(implementation = BoardDetailesCollectionResponse.class)
+            )),
+            @ApiResponse(responseCode = "403", description = "Access Denied", content = @Content(
+                    schema = @Schema(implementation = APIExceptionResponse.class)
+            ))
+    })
     @GetMapping
-    public ResponseEntity<CollectionResponse<BoardShortDto>> getBoards(
+    public ResponseEntity<BoardDetailesCollectionResponse> getBoards(
+            @Parameter(in = ParameterIn.QUERY, name = "t", description = "Team Id", schema = @Schema(implementation = UUID.class), required = true)
             @RequestParam("t") UUID teamId
     ) {
 
@@ -42,22 +62,42 @@ public class BoardsController {
         pageMeta.setTotalItems(dtos.size());
 
         return ResponseEntity.ok(
-                new CollectionResponse<>(
+                new BoardDetailesCollectionResponse(
                         dtos,
                         pageMeta
                 )
         );
     }
 
+    @Operation(
+            summary = "Get Board Details",
+            description = "Returns Details Of Specified Board"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Board Details", content = @Content(
+                    schema = @Schema(implementation = BoardDetailsResponse.class)
+            )),
+            @ApiResponse(responseCode = "400", description = "Invalid request parameters", content = @Content(
+                    schema = @Schema(implementation = APIExceptionResponse.class)
+            )),
+            @ApiResponse(responseCode = "403", description = "Access denied or insufficient permissions", content = @Content(
+                    schema = @Schema(implementation = APIExceptionResponse.class)
+            )),
+            @ApiResponse(responseCode = "404", description = "Board Not Found", content = @Content(
+                    schema = @Schema(implementation = APIExceptionResponse.class)
+            ))
+    })
     @GetMapping("/{boardId}")
-    public ResponseEntity<ApiResponse<BoardShortDto>> getBoardDetails(
+    public ResponseEntity<BoardDetailsResponse> getBoardDetails(
+            @Parameter(in = ParameterIn.QUERY, name = "t", description = "Team Id", schema = @Schema(implementation = UUID.class), required = true)
             @RequestParam("t") UUID teamId,
-            @PathVariable String boardId) {
-
+            @Parameter(in = ParameterIn.PATH, name = "boardId", description = "Board Id", schema = @Schema(implementation = String.class), required = true)
+            @PathVariable String boardId
+    ) {
         UUID userId = authUtils.authenticatedUserId();
         BoardDetailsDto dto = boardUserService.getBoardDetails(userId, boardId, teamId);
         return ResponseEntity.ok(
-                new ApiResponse<>(
+                new BoardDetailsResponse(
                         "Success",
                         dto
                 )
