bbbd4d63d007efe99346652f087cd217cecbd303
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-03-13 15:35:59 +0100

    File Node Not Found Exception Added

---
+++ backend/src/main/java/com/mordiniaa/backend/exceptions/FileNodeNotFound.java
@@ -0,0 +1,8 @@
+package com.mordiniaa.backend.exceptions;
+
+public class FileNodeNotFound extends RuntimeException {
+
+    public FileNodeNotFound(String message) {
+        super(message);
+    }
+}
