5ec4afee8ddb3728e2190829ef44e8d1c612b3a0
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-03-13 15:34:37 +0100

    Access Denied Exception Added

---
+++ backend/src/main/java/com/mordiniaa/backend/exceptions/AccessDeniedException.java
@@ -0,0 +1,7 @@
+package com.mordiniaa.backend.exceptions;
+
+public class AccessDeniedException extends RuntimeException {
+    public AccessDeniedException(String message) {
+        super(message);
+    }
+}
