5a93bb855ffa7fd6a102859b94231161e2f999d1
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-03-14 14:19:37 +0100

    Api Exception Response Documented

--- backend/src/main/java/com/mordiniaa/backend/payload/APIExceptionResponse.java
+++ backend/src/main/java/com/mordiniaa/backend/payload/APIExceptionResponse.java
@@ -1,5 +1,6 @@
 package com.mordiniaa.backend.payload;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,10 +10,16 @@ import java.time.Instant;
 @Getter
 @Setter
 @NoArgsConstructor
+@Schema(name = "Api Exception Response", description = "Schema to hold error response information")
 public class APIExceptionResponse {
 
+    @Schema(name = "status", description = "Response Status Code")
     private int status;
+
+    @Schema(name = "message", description = "Response Message")
     private String message;
+
+    @Schema(name = "timestamp", description = "Error Date")
     private Instant timestamp = Instant.now();
 
     public APIExceptionResponse(int status, String message) {
