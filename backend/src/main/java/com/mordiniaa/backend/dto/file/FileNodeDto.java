bafbc7b5d1d9d8eb61388398ef65fe162a8f5769
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-03-14 14:20:14 +0100

    File Node Dto Documented

--- backend/src/main/java/com/mordiniaa/backend/dto/file/FileNodeDto.java
+++ backend/src/main/java/com/mordiniaa/backend/dto/file/FileNodeDto.java
@@ -1,6 +1,7 @@
 package com.mordiniaa.backend.dto.file;
 
 import com.mordiniaa.backend.models.file.cloudStorage.NodeType;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,12 +11,22 @@ import java.util.UUID;
 @Getter
 @Setter
 @NoArgsConstructor
+@Schema(name = "File Node Dto", description = "Schema To Hold Object Response Information")
 public class FileNodeDto {
 
+    @Schema(name = "id", description = "FileNode Id")
     private UUID id;
+
+    @Schema(name = "parentPath", description = "Path Of The Parent Elemets")
     private String parentPath;
+
+    @Schema(name = "name", description = "Resource Name")
     private String name;
+
+    @Schema(name = "nodeType", description = "Node Type (FILE/DIR)")
     private String nodeType;
+
+    @Schema(name = "size", description = "Resource Size In Bytes")
     private Long size;
 
     //Protection to not publish ROOT
