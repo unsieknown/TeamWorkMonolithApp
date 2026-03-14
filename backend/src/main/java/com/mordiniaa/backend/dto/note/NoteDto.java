e97de269dc21b865daa96075db39264cdf52645b
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2026-03-14 14:20:09 +0100

    Note Dto Documented

--- backend/src/main/java/com/mordiniaa/backend/dto/note/NoteDto.java
+++ backend/src/main/java/com/mordiniaa/backend/dto/note/NoteDto.java
@@ -1,5 +1,6 @@
 package com.mordiniaa.backend.dto.note;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
@@ -10,14 +11,26 @@ import java.util.UUID;
 @Setter
 @SuperBuilder
 @NoArgsConstructor
+@Schema(name = "Note Dto", description = "Schema To Hold Object Response Information")
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class NoteDto {
 
+    @Schema(name = "id", description = "Note Id")
     @EqualsAndHashCode.Include
     private String id;
+
+    @Schema(name = "title", description = "Note Title")
     private String title;
+
+    @Schema(name = "ownerId", description = "Id Of The Owner")
     private UUID ownerId;
+
+    @Schema(name = "content", description = "Note Content")
     private String content;
+
+    @Schema(name = "createdAt", description = "Creation Date")
     private Instant createdAt;
+
+    @Schema(name = "updatedAt", description = "Updated Date")
     private Instant updatedAt;
 }
